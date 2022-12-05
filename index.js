041ab90e53bb0992ad2d88587ef3da9987f55366
Faith <kigenfaith20@gmail.com>
2022-12-05 22:30:03 +0300

    completed event listner assignment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
 }
+    const btn = document.getElementById("button")
+    btn.addEventListener("click",()=>{alert("Hello, I was clicked")})
+
+addingEventListener()
+// console.log
